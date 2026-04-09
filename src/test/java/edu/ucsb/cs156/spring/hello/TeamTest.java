b1f806d83c0d457e515fbcf2859a41eac94be765
Andy <Yue636@ucsb.edu>
2026-04-08 17:41:11 -0700

    aw done

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -20,7 +20,29 @@ public class TeamTest {
     }
 
    
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
 
-}
+    @Test
+    public void hashCode_returns_same_value_for_equal_objects() {
+        Team t1 = new Team("foo");
+        t1.addMember("bar");
+
+        Team t2 = new Team("foo");
+        t2.addMember("bar");
+
+        assertEquals(t1.hashCode(), t2.hashCode());
+    }
+
+    @Test
+    public void hashCode_returns_expected_value() {
+        Team t = new Team("foo");
+        t.addMember("bar");
+
+        int result = t.hashCode();
+        int expectedResult = 130294; 
+        assertEquals(expectedResult, result);
+    }
+}  
